4cede9ce5da61449f62216fb7fae241f4a00238e
yatharth kaithwar08 <kaithwary10@gmail.com>
2026-06-26 20:39:07 +0530

    Strings Common Characters

---
+++ Strings/1002_Common_characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+         vector<string> ans;
+        vector<int> commonCount(26, INT_MAX);
+        
+        
+        for (const string& word : words) {  // For each word, calculate character frequencies
+            vector<int> count(26, 0);
+            for (const char c : word)
+                ++count[c - 'a'];
+            
+            
+            for (int i = 0; i < 26; ++i) // Keep minimum frequency across all words
+                commonCount[i] = min(commonCount[i], count[i]);
+        }
+        
+        
+        for (char c = 'a'; c <= 'z'; ++c) // Build result from common counts
+            for (int i = 0; i < commonCount[c - 'a']; ++i)
+                ans.push_back(string(1, c));
+        
+        return ans;
+    }
+};
