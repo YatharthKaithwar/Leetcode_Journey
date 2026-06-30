21c2ef057c8b25ff23598361ec3b22c37e38212c
YatharthKaithwar <kaithwary10@gmail.com>
2026-06-30 20:47:33 +0530

    Time: 73 ms (61.01%), Space: 98.6 MB (52.29%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> IndexedMap; // unordered map <int,int>
+        unordered_map<int, int> IndexedMap; // unordered map <int,int> for stoing num and its index
         for (int i=0;i<nums.size();i++){ // iteration
             if(IndexedMap.find(nums[i]) != IndexedMap.end() && i - IndexedMap[nums[i]]<=k){
                 return true; // If element is already in map and their index difference is <= k
             }
             IndexedMap[nums[i]]=i; // else insert number and index in map
         }
-        return false;
+        return false; // If not find Return false
     }
 };
