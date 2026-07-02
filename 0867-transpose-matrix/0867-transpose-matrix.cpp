0a1ea62dab833514450c7906274129281d9a2026
YatharthKaithwar <kaithwary10@gmail.com>
2026-07-02 21:59:41 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.44%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+         int m = matrix.size();       // Number of rows
+        int n = matrix[0].size();    // Number of columns
+        
+        // Initialize an empty result matrix with swapped dimensions (n rows by m columns)
+        std::vector<std::vector<int>> ans(n, std::vector<int>(m));
+        
+        // Map original row and column indices to the transposed position
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        
+        return ans;;
+    }
+};
