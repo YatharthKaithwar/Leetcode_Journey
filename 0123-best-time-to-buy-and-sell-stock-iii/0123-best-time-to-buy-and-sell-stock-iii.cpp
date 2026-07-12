7003f32bfd11366a4d9c0affb621f67d9171cbe0
YatharthKaithwar <kaithwary10@gmail.com>
2026-07-12 21:18:00 +0530

    Time: 0 ms (100%), Space: 79.3 MB (88.96%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -8,10 +8,10 @@ public:
 
 
         for(int i =1; i<prices.size(); i++){
-            buying1 = max(buying1 , -prices[i]);
-            selling1 = max(selling1 , buying1 + prices[i]);
-
-            buying2 = max(buying2 , selling1 -prices[i]);
+            buying1 = max(buying1 , -prices[i]); // max of buying1 and price at ith index
+            selling1 = max(selling1 , buying1 + prices[i]); // max of selling price and (buying+price)
+ 
+            buying2 = max(buying2 , selling1 - prices[i]);
             selling2 = max(selling2 , buying2 + prices[i]);
         }
         return selling2;
