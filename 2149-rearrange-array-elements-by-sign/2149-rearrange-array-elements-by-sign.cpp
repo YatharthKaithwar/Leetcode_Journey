e5d71b71dba6a8ce24c50c81a3bcb0795b512184
YatharthKaithwar <kaithwary10@gmail.com>
2026-07-13 21:33:57 +0530

    Time: 1 ms (91.12%), Space: 128.3 MB (78.08%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -8,11 +8,11 @@ public:
         int negIndex = 1;
 
         for (int i = 0 ; i<nums.size() ; i++){
-            if(nums[i]>0){
+            if(nums[i]>0){ // if num is +ve then store it at the even index
                 ans[posIndex] = nums[i];
                 posIndex +=2;
                }
-            else{
+            else{// if num is -ve store it at odd index
                 ans[negIndex] = nums[i];
                 negIndex +=2;
             }
