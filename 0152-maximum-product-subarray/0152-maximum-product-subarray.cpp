f8aed4d9706feac09c70eb081cf905d5f4cd7871
YatharthKaithwar <kaithwary10@gmail.com>
2026-07-16 20:23:02 +0530

    Time: 0 ms (100%), Space: 17.7 MB (60.25%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int ans = nums[0];
-        for (int i =0;i<nums.size();i++){
-            int product = 1;
-            for(int j = i;j<nums.size();j++){
-                product *= nums[j];
-                ans = max(ans,product);
-                
-            }
-        }
-        return ans;
-    }
+ //       int ans = nums[0];
+//      for (int i =0;i<nums.size();i++){
+//         int product = 1;
+//         for(int j = i;j<nums.size();j++){
+//             product *= nums[j];
+//             ans = max(ans,product);            
+//            }
+//        }
+//        return ans;
+      int prefix = 1;
+      int suffix = 1;
+      int ans = INT_MIN;
+      int n = nums.size();
+      for (int i=0 ; i<n ; i++){
+        if(prefix == 0) prefix = 1;
+        if(suffix == 0) suffix = 1;
+
+        prefix = prefix*nums[i];
+        suffix = suffix*nums[n-i-1];
+        ans = max(ans,max(prefix,suffix));
+      }
+      return ans;
+      
+   }
 };
