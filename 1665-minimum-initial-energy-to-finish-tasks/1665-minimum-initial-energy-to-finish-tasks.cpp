55650a9472f3cadff26fec640f283855cb6da949
YatharthKaithwar <kaithwary10@gmail.com>
2026-07-26 21:07:49 +0530

    Time: 52 ms (47.95%), Space: 97.5 MB (46.04%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    bool isPossible(vector<vector<int>>& tasks,int mid){
-        for(auto &task : tasks){
-            int actual = task[0];
-            int minimum = task[1];
+    bool isPossible(vector<vector<int>>& tasks,int mid){// function to find possibilities
+        for(auto &task : tasks){// iteration
+            int actual = task[0];// actual energy spent
+            int minimum = task[1];// minimum required
 
-            if(minimum>mid){
-                return false;
+            if(minimum>mid){// minimum required energy is greater than the mid than it's will not to execute the work
+                return false;// not possible
             }
-
-            mid-=actual;
+            else{//  its possible
+            mid-=actual;// decreasing the energy spent from mid(total)
+        }
         }
         return true;
     }
@@ -21,7 +22,7 @@ public:
 
      int result = INT_MAX;
 
-     auto lambda = [](auto &task1, auto &task2){
+     auto lambda = [](auto &task1, auto &task2){// for finding max difference
         int diff1 = task1[1]-task1[0];
         int diff2 = task2[1]-task2[0];
         return diff1>diff2;
