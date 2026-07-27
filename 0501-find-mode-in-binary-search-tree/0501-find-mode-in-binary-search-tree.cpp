0ed741b43a5894e37dc3e88dd6780faffa94523e
YatharthKaithwar <kaithwary10@gmail.com>
2026-07-27 20:17:39 +0530

    Time: 5 ms (26.22%), Space: 25.9 MB (15.82%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,14 +11,14 @@
  */
 class Solution {
 public:
-    unordered_map <int,int> mp;
+    unordered_map <int,int> mp;// map holds {value,frequency}
 
     void inorder(TreeNode* root){
         if(!root)return;
 
         inorder(root->left);
         mp[root->val]++;
-        inorder(root->right);
+        inorder(root->right);// add all values in map
       
     }
     vector<int> findMode(TreeNode* root) {
@@ -26,11 +26,11 @@ public:
 
         vector<int> result;
         int maxFreq = 0;
-        for(auto &it : mp){
-            if(it.second > maxFreq){
-                maxFreq = it.second;
+        for(auto &it : mp){//iteration
+            if(it.second > maxFreq){// frequency is greater than the maxFrequency
+                maxFreq = it.second;// save the frequency as max Frequency
                 result = {};
-                result.push_back(it.first);
+                result.push_back(it.first);// pushing the value in the result
             }
             else if(it.second == maxFreq){
                 result.push_back(it.first);
