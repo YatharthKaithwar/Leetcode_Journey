31e87cdc4a08c0e046b9d61c5cf4a3c8a4e289f7
YatharthKaithwar <kaithwary10@gmail.com>
2026-07-28 22:01:10 +0530

    Time: 13 ms (66.81%), Space: 37.1 MB (33.49%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -5,17 +5,17 @@ public:
 
         int count = 0;
 
-        map <vector<int>, int> mp;
+        map <vector<int>, int> mp;// creating a map to store row value and freq.
 
         for(int row = 0; row<n; row++){
-            mp[grid[row]]++;
+            mp[grid[row]]++;// storing row value in map and increasing freq.
         }
         for(int  c=0; c<n; c++){
-            vector<int> temp;
+            vector<int> temp;//for storing both tow and colm value
             for(int r = 0; r<n; r++){
                 temp.push_back(grid[r][c]);
             }
-            count += mp[temp];
+            count += mp[temp];//adding the freq to count
         }
         return count;
     }
