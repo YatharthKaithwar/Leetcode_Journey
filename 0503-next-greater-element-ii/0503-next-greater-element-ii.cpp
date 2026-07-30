0c73323a98a984e3122b8aea44fbe56f8a9ebd2b
YatharthKaithwar <kaithwary10@gmail.com>
2026-07-30 20:34:34 +0530

    Time: 0 ms (100%), Space: 28.2 MB (92.42%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,14 +5,14 @@ public:
         vector<int> ans(n,-1);
         stack<int> st;
 
-        for(int i = 0;i<2*n;i++){
+        for(int i = 0;i<2*n;i++){// iterating twice for circular 
             int current_num = nums[i%n];
 
-            while(!st.empty() && nums[st.top()]<current_num){
-                ans[st.top()]=current_num;
+            while(!st.empty() && nums[st.top()]<current_num){//Resolving greater element in the stack
+                ans[st.top()]=current_num;// storing top element greater
                 st.pop();
             }
-            if(i<n){
+            if(i<n){ // Only push indices from the first pass to avoid redundancy
                 st.push(i);
             }
         }
